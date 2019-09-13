c6c00d781fce92e57cc4b5f25db942abf8d343e5
paaco <paaco@users.noreply.github.com>
2019-09-13 12:38:39 +0200

    First intro screen evah

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # c64-wheels
+
+Wheels will be game where you drive a man sitting on a single wheel across a racing parcour.
+Kinda like Kikstart.
+
+### Assets
+
+Assets are drawn in Aseprite and saved as .png file. Custom converter Sprite2asm converts the png file based on instructions in the filename:
+
+  -bgX specifies background color (defaults to transparent color index) and forces hires
+  -mcXX specifies multicolor 1 and 2 and forces multi color
+
+Note that multicolors bits are different for sprites and character sets, so mapping goes slightly different:
+
+        bits    00        01         10         11
+    sprites     BG:$D021  MC1:$D025  $D027+     MC2:$D026
+    chars       BG:$D021  MC1:$D022  MC2:$D023  $D800+
+
---
+++ wheels.asm
@@ -0,0 +1,55 @@
+;
+; wheels
+;
+
+    * = $0801
+    !byte $0c,$08,$b5,$07,$9e,$20,$32,$30,$36,$32,$00,$00,$00
+start:
+        ldx #0
+.loop_logo:
+        lda logo_charmap,x
+        sta $0400,x
+        lda #$08 ; black mc
+        sta $D800,x
+        inx
+        cpx #200
+        bne .loop_logo
+        ldx #0
+.loop_fill:
+        lda #7
+        sta $0400+200,x
+        sta $0500+200,x
+        sta $0600+200,x
+        lda #$08 ; black mc
+        sta $D800+200,x
+        sta $D900+200,x
+        sta $DA00+200,x
+        inx
+        bne .loop_fill
+
+        ; select charset at $2000
+        lda $D018
+        and #$F0
+        ora #$08 ; $2000
+        sta $D018
+
+        ; select multicolor and set colors
+        lda $D016
+        ora #$10
+        sta $D016
+
+        lda #$00
+        sta $D020
+        lda #$0B
+        sta $D021
+        lda #$09
+        sta $D022
+        lda #$08
+        sta $D023
+        rts
+
+logo_charmap:
+    !source "generated/wheels-charmap.inc"
+
+    * = $2000
+    !source "generated/wheels-charset.inc"
